6e9a8598bb678bffe90ff95cbeb2b6d900957027
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-01 18:55:28 -0500

    Clean up second solution

--- aoc2021_01/src/main.rs
+++ aoc2021_01/src/main.rs
@@ -6,15 +6,12 @@ fn main() {
         .lines()
         .map(|line| line.parse::<u32>().unwrap())
         .tuple_windows::<(_,_,_)>()
-        .tuple_windows()
+        .map(|window| window.0 + window.1 + window.2)
+        .tuple_windows::<(_,_)>()
         .fold(
             0,
-            |acc, (first, second): ((u32, u32, u32), (u32,u32,u32))| {
-                let first_sum = first.0 + first.1 + first.2;
-                let second_sum = second.0 + second.1 + second.2;
-                println!("first: {}", first_sum);
-                println!("second: {}", second_sum);
-                if first_sum < second_sum {
+            |acc, (first, second): (u32, u32)| {
+                if first < second {
                     acc + 1
                 } else {
                     acc
