ea7088ca3f01d5bcc782684a994f6681aeda6a42
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-01 18:50:17 -0500

    solve pt 2

--- aoc2021_01/src/main.rs
+++ aoc2021_01/src/main.rs
@@ -5,16 +5,22 @@ fn main() {
     let sweep_increments: u32 = sweep_depths
         .lines()
         .map(|line| line.parse::<u32>().unwrap())
+        .tuple_windows::<(_,_,_)>()
         .tuple_windows()
         .fold(
             0,
-            |acc, window: (u32, u32)| {
-                if window.0 < window.1 {
+            |acc, (first, second): ((u32, u32, u32), (u32,u32,u32))| {
+                let first_sum = first.0 + first.1 + first.2;
+                let second_sum = second.0 + second.1 + second.2;
+                println!("first: {}", first_sum);
+                println!("second: {}", second_sum);
+                if first_sum < second_sum {
                     acc + 1
                 } else {
                     acc
                 }
             },
         );
+   
     println!("{:?}", sweep_increments);
 }
