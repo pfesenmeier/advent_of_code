ca3c9ef787c4e4400a8bd990407bd10e4669a017
Paul Fesenmeier <pbfesenmeier@sep.com>
2021-12-03 11:32:39 -0500

    plan part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,14 +1,42 @@
+// pt 2
+// for every member in list
+// fold to the most common char in first place
+// reduce the list to the members starting with that char
+// if list is one, return that number
+// else, repeat 
+//  
+
+// or
+// for list
+// fold to tuple (0 occur, 1 occur)
+// reduce the list to the members starting with more common occurances
+// if list is one, return that number
+// else, repeat 
+
+// fn find_rating(tiebreaker: usize)
+// turn list into Vec<(pos, rev_list)>
+// while len > 1
+//   split into two lists with last char removed
+//     (Vec<(pos, &str)>, Vec<(pos, &str)>)
+//   return longer, breaking ties with (0 | 1)
+// return original_list[pos] as integer  // for this calculation
+
 fn main() {
     // TODO why not include_bytes?
     let input: Vec<Vec<&str>> = include_str!("input.txt")
       .lines()
+      // TODO better representation of 0 / 1s than a Vec<str> ?
+      // How about String?
       .map(|number| number.split("").collect())
+      // TODO better way to filter first and last elements of a vec??
       .map(|list: Vec<&str>| list.into_iter().filter(|entry| *entry != "").collect())
       .collect();
     
     let length = input.len();
+    // TODO - use length of first input
     let mut num_zeros = [0;12];
 
+    // TODO for pt2: a way to get hold of iterator again, to pull out chosen numbers?
     for line in &input {
         for (i,c) in line.iter().enumerate() {
             if *c == "0" {
