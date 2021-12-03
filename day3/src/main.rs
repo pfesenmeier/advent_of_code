4b376d06b73a586d9a8f704d60fccbaf8ba8498a
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-03 14:31:27 -0500

    Use String instead of Vec<str>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,18 +1,3 @@
-// pt 2
-// for every member in list
-// fold to the most common char in first place
-// reduce the list to the members starting with that char
-// if list is one, return that number
-// else, repeat 
-//  
-
-// or
-// for list
-// fold to tuple (0 occur, 1 occur)
-// reduce the list to the members starting with more common occurances
-// if list is one, return that number
-// else, repeat 
-
 // fn find_rating(tiebreaker: usize)
 // turn list into Vec<(pos, rev_list)>
 // while len > 1
@@ -22,25 +7,20 @@
 // return original_list[pos] as integer  // for this calculation
 
 fn main() {
-    // TODO why not include_bytes?
-    let input: Vec<Vec<&str>> = include_str!("input.txt")
-      .lines()
-      // TODO better representation of 0 / 1s than a Vec<str> ?
-      // How about String?
-      .map(|number| number.split("").collect())
-      // TODO better way to filter first and last elements of a vec??
-      .map(|list: Vec<&str>| list.into_iter().filter(|entry| *entry != "").collect())
-      .collect();
-    
-    let length = input.len();
-    // TODO - use length of first input
-    let mut num_zeros = [0;12];
 
-    // TODO for pt2: a way to get hold of iterator again, to pull out chosen numbers?
-    for line in &input {
-        for (i,c) in line.iter().enumerate() {
-            if *c == "0" {
-                num_zeros[i] += 1;
+    let input: Vec<&str> = include_str!("input.txt").lines().collect();
+    
+    let num_entries = input.len();
+    let line_length: usize = input[0].len();
+    let mut num_zeros_each_column: Vec<usize> = Vec::with_capacity(line_length);
+    for _i in 0..line_length {
+        num_zeros_each_column.push(0);
+    }
+
+    for line in input {
+        for (i,c) in line.chars().enumerate() {
+            if c == '0' {
+                num_zeros_each_column[i] += 1;
             }
         }
     }
@@ -48,8 +28,8 @@ fn main() {
     let mut gamma = "".to_string();
     let mut epsilon = "".to_string();
 
-    for num in num_zeros {
-        if num * 2 > length {
+    for num in num_zeros_each_column {
+        if num * 2 > num_entries {
             gamma.push('0');
             epsilon.push('1');
         } else {
@@ -61,5 +41,11 @@ fn main() {
     let gamma = usize::from_str_radix(&gamma, 2).unwrap();
     let epsilon = usize::from_str_radix(&epsilon, 2).unwrap();
 
+
     println!("{} ", gamma * epsilon);
+
+    // show how bitmasks work
+    let mask = 2_usize.pow(line_length.try_into().unwrap()) - 1;
+    let e_2 = gamma ^ mask;
+    assert_eq!(e_2, epsilon);
 }
