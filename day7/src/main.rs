fb9632fa6d1b03658f469f7d49a06c0ef74e143c
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-17 16:22:36 -0500

    day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("input.txt");
     println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
 }
 
 fn part_1(input: &str) -> u64 {
@@ -10,12 +11,37 @@ fn part_1(input: &str) -> u64 {
     let mut least_fuel = u64::MAX;
     for i in 0..max {
         let fuel = crabs
-        .iter()
-        .fold(0, |acc, crab| acc + (i64::from(*crab) - i64::from(i)).abs());
+            .iter()
+            .fold(0, |acc, crab| acc + (i64::from(*crab) - i64::from(i)).abs());
+
         if (fuel as u64) < least_fuel {
             least_fuel = fuel as u64;
         }
     }
+
+    least_fuel
+}
+
+fn part_2(input: &str) -> u64 {
+    let crabs = parser::parse(input);
+    let max = *crabs.iter().max().unwrap();
+
+    let mut least_fuel = u64::MAX;
+    for i in 0..max {
+        let fuel = crabs
+            .iter()
+            .fold(0, |acc, crab| { 
+                let distance = (i64::from(*crab) - i64::from(i) ).abs();
+
+                // triangle number
+                acc + distance * (distance + 1) / 2
+        });
+
+        if (fuel as u64) < least_fuel {
+            least_fuel = fuel as u64;
+        }
+    }
+
     least_fuel
 }
 
@@ -27,11 +53,16 @@ mod tests {
     fn part_1_should_pass_on_sample_input() {
         assert_eq!(part_1("16,1,2,0,4,2,7,1,2,14"), 37);
     }
+
+    #[test]
+    fn part_2_should_pass_on_sample_input() {
+        assert_eq!(part_2("16,1,2,0,4,2,7,1,2,14"), 168);
+    }
 }
 
 mod parser {
-   pub fn parse(input: &str) -> Vec<u32> {
-     input
+    pub fn parse(input: &str) -> Vec<u32> {
+        input
             .trim_end()
             .split(',')
             .map(str::parse::<u32>)
@@ -41,13 +72,13 @@ mod parser {
 
     #[cfg(test)]
     mod tests {
-       use super::*;
+        use super::*;
 
-       #[test]
-       fn smoke_parser() {
+        #[test]
+        fn smoke_parser() {
             for num in parse(include_str!("input.txt")) {
                 println!("{}", num);
             }
-        } 
+        }
     }
 }
