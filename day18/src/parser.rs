7c2ceab681b7b69c4214eb39580c7bcff3fbeb98
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-18 21:06:11 -0500

    Refine data structure

--- day18/src/parser.rs
+++ day18/src/parser.rs
@@ -1,7 +1,7 @@
+use crate::snailfish_number::{Expression};
 use peg;
-use crate::snailfish_number::{Expression, SnailFishNumber};
 
-pub fn parse_snailfish_number(number: &str) -> SnailFishNumber {
+pub fn parse_snailfish_number(number: &str) -> Expression {
     snailfish_number::parse(number).unwrap()
 }
 
@@ -13,10 +13,10 @@ mod tests {
     fn two_digits() {
         assert_eq!(
             parse_snailfish_number("[4,5]"),
-            SnailFishNumber {
-                lhs: Expression::Digit(4),
-                rhs: Expression::Digit(5)
-            }
+            Expression::Pair(
+                Box::new(Expression::Digit(4)),
+                Box::new(Expression::Digit(5))
+            )
         )
     }
 
@@ -24,13 +24,13 @@ mod tests {
     fn one_digit_one_pair() {
         assert_eq!(
             parse_snailfish_number("[4,[2,3]]"),
-            SnailFishNumber {
-                lhs: Expression::Digit(4),
-                rhs: Expression::Pair(
+            Expression::Pair(
+                Box::new(Expression::Digit(4)),
+                Box::new(Expression::Pair(
                     Box::new(Expression::Digit(2)),
                     Box::new(Expression::Digit(3))
-                )
-            }
+                ))
+            )
         )
     }
 
@@ -38,16 +38,16 @@ mod tests {
     fn two_pairs() {
         assert_eq!(
             parse_snailfish_number("[[4,5],[2,3]]"),
-            SnailFishNumber {
-                lhs: Expression::Pair(
+            Expression::Pair(
+                Box::new(Expression::Pair(
                     Box::new(Expression::Digit(4)),
                     Box::new(Expression::Digit(5))
-                ),
-                rhs: Expression::Pair(
+                )),
+                Box::new(Expression::Pair(
                     Box::new(Expression::Digit(2)),
                     Box::new(Expression::Digit(3))
-                )
-            }
+                ))
+            )
         )
     }
 
@@ -55,43 +55,43 @@ mod tests {
     fn sample() {
         assert_eq!(
             parse_snailfish_number("[[[[4,3],4],4],[7,[[8,4],9]]]"),
-            SnailFishNumber {
-                lhs: Expression::Pair(
+            Expression::Pair(
+                Box::new(Expression::Pair(
                     Box::new(Expression::Pair(
-                         Box::new(Expression::Pair(
-                              Box::new(Expression::Digit(4)),
-                              Box::new(Expression::Digit(3)),
-                         )),
-                         Box::new(Expression::Digit(4)),
+                        Box::new(Expression::Pair(
+                            Box::new(Expression::Digit(4)),
+                            Box::new(Expression::Digit(3)),
+                        )),
+                        Box::new(Expression::Digit(4)),
                     )),
                     Box::new(Expression::Digit(4)),
-                ),
-                rhs: Expression::Pair(
-                        Box::new(Expression::Digit(7)),
+                )),
+                Box::new(Expression::Pair(
+                    Box::new(Expression::Digit(7)),
+                    Box::new(Expression::Pair(
                         Box::new(Expression::Pair(
-                                 Box::new(Expression::Pair(
-                                    Box::new(Expression::Digit(8)),
-                                    Box::new(Expression::Digit(4)),
-                                 )),
-                                 Box::new(Expression::Digit(9)),
+                            Box::new(Expression::Digit(8)),
+                            Box::new(Expression::Digit(4)),
                         )),
-                    ),
-             }
-);
+                        Box::new(Expression::Digit(9)),
+                    )),
+                )),
+            )
+        );
     }
-
 }
 
 peg::parser! {
    grammar snailfish_number() for str {
-    pub rule parse() -> SnailFishNumber
-        = "[" lhs:expression() "," rhs:expression() "]" { SnailFishNumber { lhs, rhs }}
+    pub rule parse() -> Expression
+        = p:pair() { p }
 
     rule expression() -> Expression
         = pair() / digit()
 
     rule digit() -> Expression
         = d:$(['0'..='9']+) { Expression::Digit(d.parse().unwrap()) }
+
     rule pair() -> Expression
         = "[" lhs:expression() "," rhs:expression() "]" { Expression::Pair(Box::new(lhs), Box::new(rhs)) }
  }
--- day18/src/snailfish_number.rs
+++ day18/src/snailfish_number.rs
@@ -1,13 +1,9 @@
 #[derive(PartialEq, Debug)]
-pub struct SnailFishNumber {
-  pub lhs: Expression,
-  pub rhs: Expression,
-}
-
-#[derive(PartialEq, Debug)]
 pub enum Expression {
    Digit(u8),
    Pair(Box<Expression>, Box<Expression>)
 }
 
-impl Expression {}
+impl Expression {
+    
+}
